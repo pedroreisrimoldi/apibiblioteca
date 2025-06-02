89027ca096b2373ce798b9b4db5a187e2d4b2c92
pedro <pedroreisrimoldi@gmail.com>
2025-06-02 18:37:01 -0300

    feat: criando endpoints e service para publisher

--- exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/controllers/PublisherController.java
+++ exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/controllers/PublisherController.java
@@ -1,7 +1,69 @@
 package com.crud.exercicio.biblioteca.controllers;
 
+import com.crud.exercicio.biblioteca.controllers.dtos.PublisherCreationDto;
+import com.crud.exercicio.biblioteca.controllers.dtos.PublisherDto;
+import com.crud.exercicio.biblioteca.entities.Publisher;
+import com.crud.exercicio.biblioteca.services.PublisherService;
+import com.crud.exercicio.biblioteca.services.exceptions.PublisherNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/publishers")
 public class PublisherController {
+  private final PublisherService publisherService;
+
+  @Autowired
+  public PublisherController(PublisherService publisherService) {
+    this.publisherService = publisherService;
+  }
+
+  @GetMapping("/{id}")
+  public PublisherDto getPublisherById(@PathVariable Long id) throws PublisherNotFoundException {
+    return PublisherDto.toDto(
+            publisherService.findById(id)
+    );
+  }
+
+  @GetMapping
+  public List<PublisherDto> getAllPublishers() {
+    List<Publisher> allPublishers = publisherService.findAll();
+    return allPublishers.stream()
+            .map(PublisherDto::toDto)
+            .toList();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public PublisherDto createPublisher(@RequestBody PublisherCreationDto publisherCreationDto) {
+    return PublisherDto.toDto(
+            publisherService.create(publisherCreationDto.toEntity())
+    );
+  }
+
+  @PutMapping("/{id}")
+  public PublisherDto updatePublisher(@PathVariable Long id,
+                                      @RequestBody PublisherCreationDto publisherCreationDto) throws PublisherNotFoundException {
+    return PublisherDto.toDto(
+            publisherService.update(id, publisherCreationDto.toEntity())
+    );
+  }
+
+  @DeleteMapping("/{id}")
+  public PublisherDto deletePublisherById(@PathVariable Long id) throws PublisherNotFoundException {
+    return PublisherDto.toDto(
+            publisherService.deleteById(id)
+    );
+  }
 }
--- exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/services/PublisherService.java
+++ exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/services/PublisherService.java
@@ -1,15 +1,49 @@
 package com.crud.exercicio.biblioteca.services;
 
+import com.crud.exercicio.biblioteca.entities.Publisher;
 import com.crud.exercicio.biblioteca.entities.repositories.PublisherRepository;
+import com.crud.exercicio.biblioteca.services.exceptions.PublisherNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PublisherService {
-  private PublisherRepository publisherRepository;
+  private final PublisherRepository publisherRepository;
 
   @Autowired
   public PublisherService(PublisherRepository publisherRepository) {
     this.publisherRepository = publisherRepository;
+  }
+
+  public Publisher findById(Long id) throws PublisherNotFoundException {
+    return publisherRepository.findById(id)
+            .orElseThrow(PublisherNotFoundException::new);
+  }
+
+  public List<Publisher> findAll() {
+    return publisherRepository.findAll();
+  }
+
+  public Publisher create(Publisher publisher) {
+    return publisherRepository.save(publisher);
+  }
+
+  public Publisher update(Long id, Publisher publisher) throws PublisherNotFoundException {
+    Publisher publisherFromDb = findById(id);
+
+    publisherFromDb.setName(publisher.getName());
+    publisherFromDb.setAddress(publisher.getAddress());
+
+    return publisherRepository.save(publisherFromDb);
+  }
+
+  public Publisher deleteById(Long id) throws PublisherNotFoundException {
+    Publisher publisher = findById(id);
+
+    publisherRepository.deleteById(id);
+
+    return publisher;
   }
 }
