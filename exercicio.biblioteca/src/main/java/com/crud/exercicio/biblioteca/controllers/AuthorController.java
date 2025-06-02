e8b6c622ce9e72d2c342111a03709ce8d76d7db0
pedro <pedroreisrimoldi@gmail.com>
2025-06-02 18:35:37 -0300

    feat: criando endpoints author e service

--- exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/controllers/AuthorController.java
+++ exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/controllers/AuthorController.java
@@ -1,7 +1,69 @@
 package com.crud.exercicio.biblioteca.controllers;
 
+import com.crud.exercicio.biblioteca.controllers.dtos.AuthorCreationDto;
+import com.crud.exercicio.biblioteca.controllers.dtos.AuthorDto;
+import com.crud.exercicio.biblioteca.entities.Author;
+import com.crud.exercicio.biblioteca.services.AuthorService;
+import com.crud.exercicio.biblioteca.services.exceptions.AuthorNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/authors")
 public class AuthorController {
+  private final AuthorService authorService;
+
+  @Autowired
+  public AuthorController(AuthorService authorService) {
+    this.authorService = authorService;
+  }
+
+  @GetMapping("/{id}")
+  public AuthorDto getAuthorById(@PathVariable Long id) throws AuthorNotFoundException {
+    return AuthorDto.toDto(
+            authorService.findById(id)
+    );
+  }
+
+  @GetMapping
+  public List<AuthorDto> getAllAuthors() {
+    List<Author> allAuthors = authorService.findAll();
+    return allAuthors.stream()
+            .map(AuthorDto::toDto)
+            .toList();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public AuthorDto createAuthor(@RequestBody AuthorCreationDto authorCreationDto) {
+    return AuthorDto.toDto(
+            authorService.create(authorCreationDto.toEntity())
+    );
+  }
+
+  @PutMapping("/{id}")
+  public AuthorDto updateAuthor(@PathVariable Long id,
+                                @RequestBody AuthorCreationDto authorCreationDto) throws AuthorNotFoundException {
+    return AuthorDto.toDto(
+            authorService.update(id, authorCreationDto.toEntity())
+    );
+  }
+
+  @DeleteMapping("/{id}")
+  public AuthorDto deleteAuthorById(@PathVariable Long id) throws AuthorNotFoundException {
+    return AuthorDto.toDto(
+            authorService.deleteById(id)
+    );
+  }
 }
--- exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/services/AuthorService.java
+++ exercicio.biblioteca/src/main/java/com/crud/exercicio/biblioteca/services/AuthorService.java
@@ -1,15 +1,48 @@
 package com.crud.exercicio.biblioteca.services;
 
+import com.crud.exercicio.biblioteca.entities.Author;
 import com.crud.exercicio.biblioteca.entities.repositories.AuthorRepository;
+import com.crud.exercicio.biblioteca.services.exceptions.AuthorNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuthorService {
-  private AuthorRepository authorRepository;
+  private final AuthorRepository authorRepository;
 
   @Autowired
   public AuthorService(AuthorRepository authorRepository) {
     this.authorRepository = authorRepository;
+  }
+
+  public Author findById(Long id) throws AuthorNotFoundException {
+    return authorRepository.findById(id).orElseThrow(AuthorNotFoundException::new);
+  }
+
+  public List<Author> findAll() {
+    return authorRepository.findAll();
+  }
+
+  public Author create(Author author) {
+    return authorRepository.save(author);
+  }
+
+  public Author update(Long id, Author author) throws AuthorNotFoundException {
+    Author authorFromDb = findById(id);
+
+    authorFromDb.setName(author.getName());
+    authorFromDb.setNationality(author.getNationality());
+
+    return authorRepository.save(authorFromDb);
+  }
+
+  public Author deleteById(Long id) throws AuthorNotFoundException {
+    Author author = findById(id);
+
+    authorRepository.deleteById(id);
+
+    return author;
   }
 }
